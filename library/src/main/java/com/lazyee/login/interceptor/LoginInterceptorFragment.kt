a1ea5cc61c1199639e0d71e910bf1337628c2c31
leeorz <378229364@qq.com>
2021-03-18 18:22:59 +0800

    移除kotlin-android-extensions

--- library/src/main/java/com/lazyee/login/interceptor/LoginInterceptorFragment.kt
+++ library/src/main/java/com/lazyee/login/interceptor/LoginInterceptorFragment.kt
@@ -3,14 +3,8 @@ package com.lazyee.login.interceptor
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
-import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
+import com.lazyee.login.interceptor.LoginInterceptorFragment.Companion.TAG
 
 /**
  * fragment 登录拦截Fragment
